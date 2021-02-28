9478143e33424fdcd9cc8917bd23617c4de3ff58
Grzegorz <grzp91@gmail.com>
2021-02-28 10:13:25 +0100

    Normal state

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -6,8 +6,6 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody Rigidbody;
 
-    [SerializeField] private float m_speed;
-    [SerializeField] private float m_hight;
     void Start()
     {
         
@@ -19,18 +17,10 @@ public class NewBehaviourScript : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             Debug.Log("Nacisnałem A");
-
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.left * m_speed);
         }
         if(Input.GetKey(KeyCode.D))
         {
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.right * m_speed);
-
             Debug.Log("Nacisnałem D");
-        }
-        if(Input.GetKey(KeyCode.W))
-        {
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.up * m_hight);
         }
     }
 }
