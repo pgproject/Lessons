06da1ccdc05e67428b964896d32d1f9e5b407f31
Grzegorz <grzp91@gmail.com>
2021-02-28 18:01:52 +0100

    Next part- walking

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody Rigidbody;
-
+    public float Speed;
+    public float HeigthOfJump;
     void Start()
     {
         
@@ -17,10 +18,17 @@ public class NewBehaviourScript : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             Debug.Log("Nacisnałem A");
+            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.left * Speed);
         }
         if(Input.GetKey(KeyCode.D))
         {
             Debug.Log("Nacisnałem D");
+            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.right * Speed);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.up * HeigthOfJump);
+            Debug.Log("Odcisnałem W");
         }
     }
 }
