03a8a2cc67122b957df183915520ed3d262369cc
Grzegorz <grzp91@gmail.com>
2021-02-28 10:20:47 +0100

    Next step

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -8,6 +8,8 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] private float m_speed;
     [SerializeField] private float m_hight;
+
+    private bool isGround;
     void Start()
     {
         
@@ -28,9 +30,25 @@ public class NewBehaviourScript : MonoBehaviour
 
             Debug.Log("Nacisnałem D");
         }
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W) && isGround)
         {
             Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.up * m_hight);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.name== "Ground")
+        {
+            isGround = true;
+            Debug.Log("1234");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "Ground")
+        {
+            isGround = false;
         }
     }
 }
