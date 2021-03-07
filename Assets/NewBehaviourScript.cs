8f7e2a13123620a338b0b11113f7f1c6386e451f
Grzegorz <grzp91@gmail.com>
2021-03-07 16:59:13 +0100

    2d

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,50 +4,61 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Rigidbody Rigidbody;
+    public Rigidbody2D Rigidbody;
 
     [SerializeField] private float m_speed;
     [SerializeField] private float m_hight;
-
+    private float m_speedHelp;
     private bool isGround;
-    void Start()
+    [SerializeField] private float m_moveDown;
+    void Awake()
     {
-        
+        m_speedHelp = m_speed;
     }
 
    
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.A))
         {
             Debug.Log("Nacisnałem A");
-
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.left * m_speed);
+            if(isGround)
+                Rigidbody.MovePosition(Rigidbody.position + Vector2.left * m_speed * Time.fixedDeltaTime);
+            else
+            {
+                Rigidbody.MovePosition(new Vector2(Rigidbody.position.x -1 * m_speed * Time.fixedDeltaTime, Rigidbody.position.y + m_moveDown * Time.fixedDeltaTime));
+            }
         }
         if(Input.GetKey(KeyCode.D))
         {
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.right * m_speed);
+            if (isGround)
+                Rigidbody.MovePosition(Rigidbody.position + Vector2.right * m_speed * Time.fixedDeltaTime);
 
+            else
+            {
+                Rigidbody.MovePosition(new Vector2(Rigidbody.position.x + 1 * m_speed * Time.fixedDeltaTime, Rigidbody.position.y + m_moveDown * Time.fixedDeltaTime));
+            }
             Debug.Log("Nacisnałem D");
         }
         if(Input.GetKey(KeyCode.W) && isGround)
         {
-            Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.up * m_hight);
+            Rigidbody.MovePosition(Rigidbody.position + Vector2.up * m_hight * Time.fixedDeltaTime);
         }
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.gameObject.name== "Ground")
-        {
-            isGround = true;
-            Debug.Log("1234");
-        }
-    }
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.name == "Ground")
         {
+            isGround = true;
+            m_speed = m_speedHelp;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name == "Ground")
+        {
+            m_speed /= 10;
             isGround = false;
         }
     }
