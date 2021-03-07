c6e258a13e566ed5b1d15bb29146ff7f5fc9162c
Grzegorz <grzp91@gmail.com>
2021-03-07 18:03:03 +0100

    Next step

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -7,9 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     public Rigidbody Rigidbody;
     public float Speed;
     public float HeigthOfJump;
+    public float SpeedInAir;
+
+    private float m_speedHelp;
+    private bool m_isGround;
+    
+
     void Start()
     {
-        
+        m_speedHelp = Speed;
     }
 
    
@@ -25,10 +31,30 @@ public class NewBehaviourScript : MonoBehaviour
             Debug.Log("Nacisnałem D");
             Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.right * Speed);
         }
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.Space) && m_isGround)
         {
             Rigidbody.MovePosition(Rigidbody.transform.position + Vector3.up * HeigthOfJump);
             Debug.Log("Odcisnałem W");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Ground")
+        {
+            Speed = m_speedHelp;
+            m_isGround = true;
+            Debug.Log("Spadłem na ziemię");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.collider.name == "Ground")
+        {
+            Speed /= SpeedInAir;
+            m_isGround = false;
+            Debug.Log("Skoczyłem");
         }
     }
 }
