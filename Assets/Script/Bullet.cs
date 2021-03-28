288162ce805f936d373692db78f71eb83e2413d8
Grzegorz <grzp91@gmail.com>
2021-03-28 17:37:53 +0200

    Commit all files

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody m_rigidbody;
+    [SerializeField] private float m_speed;
+
+    [SerializeField] private float m_distacnceToDestroy;
+
+    private float m_distanceTraveled;
+
+    void Update()
+    {
+        m_rigidbody.MovePosition(new Vector3(transform.position.x + m_speed, transform.position.y, transform.position.z));
+
+        m_distanceTraveled += m_speed;
+
+
+    }
+}
---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private GameObject m_bullet;
+    [SerializeField] private Transform m_playerTransform;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Instantiate(m_bullet, new Vector3(m_playerTransform.position.x + 1f, m_playerTransform.position.y + 0.5f, m_playerTransform.position.z), m_playerTransform.rotation);
+        }
+    }
+}
