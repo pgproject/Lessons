58030054d069def759ac3276bb8fb9cf8ac4dc7a
Grzegorz <grzp91@gmail.com>
2021-03-28 21:19:01 +0200

    Add files

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,16 +8,26 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody m_rigidbody;
     [SerializeField] private float m_speed;
 
-    [SerializeField] private float m_distacnceToDestroy;
-
-    private float m_distanceTraveled;
+    [SerializeField] private float m_time;
+    [SerializeField] private int m_damage;
+    private void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
 
     void Update()
     {
         m_rigidbody.MovePosition(new Vector3(transform.position.x + m_speed, transform.position.y, transform.position.z));
+    }
 
-        m_distanceTraveled += m_speed;
+    private IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(m_time);
+        Destroy(this.gameObject);
+    }
 
-
+    private void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.GetComponent<IDamage>()?.Damage(m_damage);
     }
 }
